d06e86d7740c3204f67687fb041067a496fc7445
Nourhene <nourhene.hadhri@esprit.tn>
2017-01-25 16:40:12 +0100

    configuration "comminucation entre backend & frontend
    ----

--- server.js
+++ server.js
@@ -213,7 +213,11 @@ router.route("/users/:id")
         });
         });
     
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/',router);
 
 app.listen(3000);
